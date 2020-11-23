fa4097d08bb0ddab7b0381521868e6d12b2a4323
carolina <carolina.santiago.t@hotmail.com>
2020-11-23 14:26:21 +0000

    adjusted

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ApplicationRecord
+    has_many :ingredients, inverse_of: :recipe
+    has_many :directions
+
+    accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
+    accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
+
+    validates :title, :description, :image, presence: true
+    
     has_attached_file :image, validate_media_type: false, styles: { medium: "400x400#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
